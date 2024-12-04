9b2eb576be01a376e51e0f40c005a45babff3749
kabinetleonova <kabinetleonova@bk.ru>
2024-12-04 13:02:16 +0300

    add lesson8

--- lesson8/index.php
+++ lesson8/index.php
@@ -8,6 +8,32 @@
     <title>PHP lesson 8</title>
 </head>
 <body>
+<?php
+    // одномерные
+    $numbers = array(5, 4, 23, -55, 9);
+    $numbers[1] = 0;
+    echo $numbers[0] . '<br>';
+    echo $numbers[1] . '<br>';
 
+    $arr = array('Privet', 6, 29.42, true);
+    $arr[3] = false;
+    echo $arr[0] . '<br>';
+
+    // Ассоциативные
+    $list = ["age" => 50, "name" => "Alex", "hobby" => "draw", 2 => 532];
+    $list["name"] = "Bob";
+    echo $list['name'] . '<br>';
+    $list[2] = 555;
+    echo $list[2] . '<br>';
+
+    // Многомерные
+    $matrix = [
+            ["1", 2, 3],
+            [4, true],
+            [7, 8, '<h4>тег из массива</h4>', 2.5]
+    ];
+    echo $matrix[2][2] . '<br>';
+
+?>
 </body>
 </html>
